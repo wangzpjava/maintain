e9c17202c699b4aa81336c3d6e4acda3a196669d
Wang Zhengpeng <wang_zhengpeng@wuxiapptec.com>
2018-05-16 16:57:56 +0800

    修改pom文件

--- src/main/java/com/maintain/Maintain.java
+++ src/main/java/com/maintain/Maintain.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LnInstrumentApp {
+public class Maintain {
 
     public static void main(String[] args){
-        SpringApplication.run(LnInstrumentApp.class, args);
+        SpringApplication.run(Maintain.class, args);
     }
 }
